66f155389497e626949cc0fdad4ea005eff4ab82
Karl <80837432+Indecis1@users.noreply.github.com>
2023-08-04 15:37:51 +0200

    #2 Add unique player and equality function

--- src/player.py
+++ src/player.py
@@ -4,18 +4,20 @@ from random import Random
 
 class Player:
 
+    POSSIBLE_IDS = list(range(1, 10))
     def __init__(self, name: str, color: str, token: str = "Circle"):
         self.name = name
         self.color = color
         self.token = token
-        self.possible_ids = list(range(1, 10))
         self.id = None
         self.__init_id()
 
     def __init_id(self):
         ran = Random()
         ran.randint(1, 10)
-        choice_id = ran.choice(self.possible_ids)
-        self.possible_ids.remove(choice_id)
+        choice_id = ran.choice(self.POSSIBLE_IDS)
+        Player.POSSIBLE_IDS.remove(choice_id)
         self.id = choice_id
 
+    def __eq__(self, other):
+        return self.id == other.id
